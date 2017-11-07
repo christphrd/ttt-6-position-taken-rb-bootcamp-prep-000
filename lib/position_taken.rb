e76d4d4a93aa7aafee3a8682e29b27aabbc4a372
Christopher Diep <diep.christopher@gmail.com>
2017-11-07 01:24:14 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,6 +2,6 @@
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     return false
-  else board[index] == "X" || "O"
+  else board[index] == "X" || board[index] == "O"
     return true
 end
